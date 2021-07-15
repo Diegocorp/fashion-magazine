ec2d1107f10e9f0695a1e1d51da40b89222a54ef
Diegocorp <diegosandovalr.corp@gmail.com>
2021-07-14 19:22:29 -0700

    backend settings

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const PORT = 5000;
 
-app.listen("5000", () => {
-  console.log("Backend is online...");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_STRING_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Conected to MONGOBD..."))
+  .catch((err: any) => console.log(err));
+
+app.use("/", (req: any, res: any) => {
+  console.log("This is the main URL");
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
 });
